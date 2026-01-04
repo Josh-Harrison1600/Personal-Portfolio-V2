b08b566f9814b66d788b2dc819160a6d038328e6
Josh Harrison <42451270+Josh-Harrison1600@users.noreply.github.com>
2026-01-04 14:23:37 -0400

    added typing effect on landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,18 +1,63 @@
 import "./Landing.css";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import Typewriter from "typewriter-effect";
 
 export default function Landing({
 	onScrollToAbout,
 }: {
 	onScrollToAbout: () => void;
 }) {
+	const [introFinished, setIsIntroFinished] = useState(false);
 	const { t } = useTranslation();
+	const roles = t("landing.role-array", { returnObjects: true });
+
 	return (
-		<div>
-			<h2>{t("landing.greeting")}</h2>
-			<button type="button" onClick={onScrollToAbout}>
-				Get Started
-			</button>
+		<div className="greeting">
+			{/* Hello I'm Josh */}
+			<div className="greeting-typewriter">
+				<Typewriter
+					onInit={(typewriter) => {
+						typewriter
+							.typeString(
+								`${t("landing.greeting-one")}<span style='color: red;'>${t("landing.greeting-two")}</span>${t("landing.greeting-three")}`,
+							)
+							.pauseFor(2500)
+							.start()
+							.callFunction(() => {
+								setIsIntroFinished(true);
+							});
+					}}
+				/>
+			</div>
+
+			<div className="role-array">
+				{/* I'm a */}
+				<div className="role-state">
+					{introFinished && (
+						<Typewriter
+							options={{
+								loop: true,
+							}}
+							onInit={(typewriter) => {
+								typewriter;
+								if (Array.isArray(roles)) {
+									roles.forEach((role) => {
+										typewriter.typeString(role).pauseFor(150).deleteAll();
+									});
+								}
+
+								typewriter.start();
+							}}
+						/>
+					)}
+				</div>
+			</div>
+			<div className="get-started">
+				<button type="button" onClick={onScrollToAbout}>
+					Get Started
+				</button>
+			</div>
 		</div>
 	);
 }
