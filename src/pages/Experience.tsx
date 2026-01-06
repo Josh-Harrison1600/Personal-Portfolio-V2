cee1508658bee2bcfe925cdcffc87c7020f55699
Josh Harrison <42451270+Josh-Harrison1600@users.noreply.github.com>
2026-01-06 18:03:42 -0400

    added redirect to company sites

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -26,84 +26,99 @@ export default function Experience() {
 
 			<div className="experience-content">
 				{/* General Data */}
+
 				<div className="general-data">
-					<div className="company-information">
-						<h2 className="year-h2">2024 - Current</h2>
-						<div className="company-title">
-							<h3>
-								<FaRegBuilding />
-								General Data
-							</h3>
-							<h3>
-								<FaLaptop />
-								Junior Full-Stack Developer
-							</h3>
-							<h3>
-								<FaLocationDot />
-								{t("experience.moncton")}
-								<img
-									className="country-img"
-									src="https://purecatamphetamine.github.io/country-flag-icons/3x2/CA.svg"
-									alt="Canada"
-								/>
-							</h3>
+					<a
+						className="company-redirect"
+						href="https://generaldata.ca/"
+						target="__blank"
+						rel="noopener"
+					>
+						<div className="company-information">
+							<h2 className="year-h2">2024 - Current</h2>
+							<div className="company-title">
+								<h3>
+									<FaRegBuilding />
+									General Data
+								</h3>
+								<h3>
+									<FaLaptop />
+									Junior Full-Stack Developer
+								</h3>
+								<h3>
+									<FaLocationDot />
+									{t("experience.moncton")}
+									<img
+										className="country-img"
+										src="https://purecatamphetamine.github.io/country-flag-icons/3x2/CA.svg"
+										alt="Canada"
+									/>
+								</h3>
+							</div>
+							<p className="company-p">{t("experience.general-data")}</p>
+							<div className="skill-list">
+								<ExperienceCard title="React" />
+								<ExperienceCard title="CSS" />
+								<ExperienceCard title="Tailwind" />
+								<ExperienceCard title="Git" />
+								<ExperienceCard title="Nodejs" />
+								<ExperienceCard title="Github Actions" />
+								<ExperienceCard title="Azure" />
+								<ExperienceCard title="MongoDB" />
+								<ExperienceCard title="Vite" />
+							</div>
 						</div>
-						<p className="company-p">{t("experience.general-data")}</p>
-						<div className="skill-list">
-							<ExperienceCard title="React" />
-							<ExperienceCard title="CSS" />
-							<ExperienceCard title="Tailwind" />
-							<ExperienceCard title="Git" />
-							<ExperienceCard title="Nodejs" />
-							<ExperienceCard title="Github Actions" />
-							<ExperienceCard title="Azure" />
-							<ExperienceCard title="MongoDB" />
-							<ExperienceCard title="Vite" />
-						</div>
-					</div>
+					</a>
 				</div>
 
 				{/* HENNGE */}
 				<div className="hennge">
-					<div className="company-information">
-						<h2 className="year-h2">2025</h2>
-						<div className="company-title">
-							<h3>
-								<FaRegBuilding />
-								HENNGE
-							</h3>
-							<h3>
-								<FaLaptop />
-								Front-end Developer Intern
-							</h3>
-							<h3>
-								<FaLocationDot />
-								{t("experience.tokyo")}
-								<img
-									className="country-img"
-									src="https://purecatamphetamine.github.io/country-flag-icons/3x2/JP.svg"
-									alt="Japan"
-								/>
-							</h3>
+					<a
+						className="company-redirect"
+						href="https://hennge.com/global/"
+						target="__blank"
+						rel="noopener"
+					>
+						<div className="company-information">
+							<h2 className="year-h2">2025</h2>
+							<div className="company-title">
+								<h3>
+									<FaRegBuilding />
+									HENNGE
+								</h3>
+								<h3>
+									<FaLaptop />
+									Front-end Developer Intern
+								</h3>
+								<h3>
+									<FaLocationDot />
+									{t("experience.tokyo")}
+									<img
+										className="country-img"
+										src="https://purecatamphetamine.github.io/country-flag-icons/3x2/JP.svg"
+										alt="Japan"
+									/>
+								</h3>
+							</div>
+							<p className="company-p">{t("experience.hennge")}</p>
+							<div className="skill-list">
+								<ExperienceCard title="React" />
+								<ExperienceCard title="TypeScript" />
+								<ExperienceCard title="CSS" />
+								<ExperienceCard title="Git" />
+								<ExperienceCard title="GitHub Actions" />
+								<ExperienceCard title="AWS" />
+								<ExperienceCard title="React Query" />
+								<ExperienceCard title="Terraform" />
+								<ExperienceCard title="S3" />
+								<ExperienceCard title="Route53" />
+								<ExperienceCard title="CloudFront" />
+								<ExperienceCard title="Vite" />
+								<ExperienceCard title="Vitest" />
+								<ExperienceCard title="OAuth" />
+							</div>
 						</div>
-						<p className="company-p">{t("experience.hennge")}</p>
-						<div className="skill-list">
-							<ExperienceCard title="React" />
-							<ExperienceCard title="TypeScript" />
-							<ExperienceCard title="CSS" />
-							<ExperienceCard title="Git" />
-							<ExperienceCard title="GitHub Actions" />
-							<ExperienceCard title="AWS" />
-							<ExperienceCard title="React Query" />
-							<ExperienceCard title="Terraform" />
-							<ExperienceCard title="S3" />
-							<ExperienceCard title="Route53" />
-							<ExperienceCard title="CloudFront" />
-							<ExperienceCard title="Vite" />
-							<ExperienceCard title="Vitest" />
-							<ExperienceCard title="OAuth" />
-						</div>
-					</div>
+					</a>
 				</div>
 			</div>
 		</div>
