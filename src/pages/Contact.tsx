31e5aa5c5b6bf56f9aa171934d1fffdfff2c0684
Josh Harrison <42451270+Josh-Harrison1600@users.noreply.github.com>
2026-02-18 18:01:56 -0400

    Added framer motion to contact and projects

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,6 @@
 import "./Contact.css";
 import emailjs from "@emailjs/browser";
+import { motion, type Variants } from "motion/react";
 import { type SubmitHandler, useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { MdOutlineEmail } from "react-icons/md";
@@ -39,35 +40,58 @@ export default function Contact() {
 		}
 	};
 
+	const fadeInUp: Variants = {
+		hidden: {
+			opacity: 0,
+			y: 60,
+		},
+		visible: {
+			opacity: 1,
+			y: 0,
+			transition: {
+				duration: 0.8,
+				ease: "easeOut",
+			},
+		},
+	};
+
 	return (
-		<div className="contact-div">
-			<div className="contact-header">
-				<h2>{t("contact.header")}</h2>
-			</div>
-			<div className="contact-methods">
-				<div className="email-form">
-					<h2>{t("contact.reach-out")}</h2>
-					<form onSubmit={handleSubmit(sendEmail)}>
-						<input placeholder={t("contact.name")} {...register("name")} />
-						<input placeholder={t("contact.email")} {...register("email")} />
-						<textarea
-							placeholder={t("contact.message")}
-							{...register("message")}
-						/>
-						<button type="submit" className="submit-message-btn">
-							<MdOutlineEmail className="email-icon" />
-							{t("contact.send-msg")}
-						</button>
-					</form>
-					<div className="socials"></div>
+		<motion.div
+			className="contact-div"
+			variants={fadeInUp}
+			initial="hidden"
+			whileInView="visible"
+			viewport={{ once: true, amount: 0.2 }}
+		>
+			<div className="contact-div">
+				<div className="contact-header">
+					<h2>{t("contact.header")}</h2>
 				</div>
+				<div className="contact-methods">
+					<div className="email-form">
+						<h2>{t("contact.reach-out")}</h2>
+						<form onSubmit={handleSubmit(sendEmail)}>
+							<input placeholder={t("contact.name")} {...register("name")} />
+							<input placeholder={t("contact.email")} {...register("email")} />
+							<textarea
+								placeholder={t("contact.message")}
+								{...register("message")}
+							/>
+							<button type="submit" className="submit-message-btn">
+								<MdOutlineEmail className="email-icon" />
+								{t("contact.send-msg")}
+							</button>
+						</form>
+						<div className="socials"></div>
+					</div>
+				</div>
+				<ToastContainer
+					position="top-right"
+					autoClose={3000}
+					style={{ zIndex: 999999 }}
+					toastClassName="custom-toast"
+				/>
 			</div>
-			<ToastContainer
-				position="top-right"
-				autoClose={3000}
-				style={{ zIndex: 999999 }}
-				toastClassName="custom-toast"
-			/>
-		</div>
+		</motion.div>
 	);
 }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,5 @@
 import "./Projects.css";
+import { motion, type Variants } from "motion/react";
 import { useTranslation } from "react-i18next";
 import { GoArrowUpRight } from "react-icons/go";
 import SCRAPER from "../assets/japan-job-scraper.png";
@@ -19,32 +20,75 @@ function ProjectCard({ title }: ProjectCardProps) {
 export default function Projects() {
 	const { t } = useTranslation();
 
+	const bounceInLeft: Variants = {
+		hidden: {
+			opacity: 0,
+			x: -100,
+		},
+		visible: {
+			opacity: 1,
+			x: 0,
+			transition: {
+				type: "spring",
+				stiffness: 80,
+				damping: 5,
+				mass: 0.75,
+				velocity: 1,
+			},
+		},
+	};
+
+	const bounceInRight: Variants = {
+		hidden: {
+			opacity: 0,
+			x: 100,
+		},
+		visible: {
+			opacity: 1,
+			x: 0,
+			transition: {
+				type: "spring",
+				stiffness: 80,
+				damping: 5,
+				mass: 0.75,
+				velocity: 1,
+			},
+		},
+	};
+
 	return (
 		<div className="projects-content">
 			<div className="projects-header">
 				<h2>{t("projects.header")}</h2>
 			</div>
 			<div className="project-1">
-				<div className="project-title-and-description">
-					<a
-						href="https://kana-typer-fawn.vercel.app/"
-						target="__blank"
-						rel="noopener noreferrer"
-						className="project-title"
-					>
-						{t("projects.kana-typer-title")}{" "}
-						<GoArrowUpRight className="arrow" size={24} />
-					</a>
-					<p className="project-description">
-						{t("projects.kana-typer-description")}
-					</p>
-					<div className="stack-used">
-						<ProjectCard title="React" />
-						<ProjectCard title="CSS" />
-						<ProjectCard title="Tailwind" />
-						<ProjectCard title="TypeScript" />
+				<motion.div
+					variants={bounceInLeft}
+					initial="hidden"
+					whileInView="visible"
+					viewport={{ once: true, amount: 0.5 }}
+				>
+					<div className="project-title-and-description">
+						<a
+							href="https://kana-typer-fawn.vercel.app/"
+							target="__blank"
+							rel="noopener noreferrer"
+							className="project-title"
+						>
+							{t("projects.kana-typer-title")}{" "}
+							<GoArrowUpRight className="arrow" size={24} />
+						</a>
+						<p className="project-description">
+							{t("projects.kana-typer-description")}
+						</p>
+						<div className="stack-used">
+							<ProjectCard title="React" />
+							<ProjectCard title="CSS" />
+							<ProjectCard title="Tailwind" />
+							<ProjectCard title="TypeScript" />
+						</div>
 					</div>
-				</div>
+				</motion.div>
 				<div className="project-img-div">
 					<img src={KANATYPER} className="project-img" alt="Kana Typer" />
 				</div>
@@ -53,27 +97,34 @@ export default function Projects() {
 				<div className="project-img-div">
 					<img src={SCRAPER} className="project-img" alt="Japan Job Scraper" />
 				</div>
-				<div className="project-title-and-description">
-					<a
-						href="https://github.com/Josh-Harrison1600/Japan-Job-Scraper"
-						target="__blank"
-						rel="noopener noreferrer"
-						className="project-title-bottom"
-					>
-						{t("projects.japan-job-scraper-title")}{" "}
-						<GoArrowUpRight className="arrow" size={24} />
-					</a>
-					<p className="project-description">
-						{t("projects.japan-job-scraper")}
-					</p>
-					<div className="stack-used">
-						<ProjectCard title="Python" />
-						<ProjectCard title="React" />
-						<ProjectCard title="CSS" />
-						<ProjectCard title="TypeScript" />
-						<ProjectCard title="GitHub Actions" />
+				<motion.div
+					variants={bounceInRight}
+					initial="hidden"
+					whileInView="visible"
+					viewport={{ once: true, amount: 0.5 }}
+				>
+					<div className="project-title-and-description">
+						<a
+							href="https://github.com/Josh-Harrison1600/Japan-Job-Scraper"
+							target="__blank"
+							rel="noopener noreferrer"
+							className="project-title-bottom"
+						>
+							{t("projects.japan-job-scraper-title")}{" "}
+							<GoArrowUpRight className="arrow" size={24} />
+						</a>
+						<p className="project-description">
+							{t("projects.japan-job-scraper")}
+						</p>
+						<div className="stack-used">
+							<ProjectCard title="Python" />
+							<ProjectCard title="React" />
+							<ProjectCard title="CSS" />
+							<ProjectCard title="TypeScript" />
+							<ProjectCard title="GitHub Actions" />
+						</div>
 					</div>
-				</div>
+				</motion.div>
 			</div>
 		</div>
 	);
